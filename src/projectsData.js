a42a85f687dc854081eff8d2ffb0d511ae6acb51
ChakornK <120770627+ChakornK@users.noreply.github.com>
2025-09-06 22:23:25 -0700

    feat: add project descriptions

--- src/projectsData.js
+++ src/projectsData.js
@@ -3,11 +3,14 @@ export const projects = [
     title: "JH Eagles",
     id: "jheagles",
     brief:
-      "A remake of Johnston Height's EagleTime app that follows Material Design 3, made with Flutter",
+      "MD3-compliant remake of Johnston Height's EagleTime app, made with Flutter",
     features: [
       "Built with Flutter and Material Design 3 component widgets",
       "Connects to the school's EagleTime app's API",
+      "Supports light/dark mode and uses the system's accent color",
       "Home page allows users to view general information such as the current weather and block rotation",
+      "Messages page allows users to view messages from the school's EagleTime app",
+      "Calendar page allows users to view upcoming events from the school's EagleTime app",
     ],
     imgsNum: 4,
     links: {
@@ -18,7 +21,13 @@ export const projects = [
     title: "Drawdle",
     id: "drawdle",
     brief: "A website to teach drawing",
-    features: [],
+    features: [
+      "Made in collaboration with two other people",
+      "Built with React, Pixi.js, and Tailwind CSS",
+      "Full-stack app powered by Next.js",
+      "Frontend provides a drawing canvas with customizable brush sizes and colors",
+      "Backend handles the storage of the user's drawing data",
+    ],
     imgsNum: 2,
     links: {
       github: "https://github.com/drawdle/drawdle",
@@ -29,7 +38,10 @@ export const projects = [
     id: "svglg",
     brief:
       "Simple Codepen project that shows how the liquid glass effect can be created using SVG filters",
-    features: [],
+    features: [
+      "Displacement map is dynamically generated with SVG filters and applied to the desired HTML element",
+      "Allows the liquid glass effect to be applied to any HTML element",
+    ],
     imgs: ["svglgcover"],
     links: {
       codepen: "https://codepen.io/chakornk/pen/ZYbRrgZ",
@@ -39,7 +51,15 @@ export const projects = [
     title: "ClubManager",
     id: "clubmanager",
     brief: "Website for managing school clubs",
-    features: [],
+    features: [
+      "Made in collaboration with one other person",
+      "Built with React and Tailwind CSS",
+      "Fontend powered by Next.js",
+      "Discover page allows users to explore and join clubs",
+      "My clubs page allows users to view clubs they have joined and manage clubs they have created",
+      "Club details page shows information about the club: its members, owners, sponsor teachers, location, and meeting schedule",
+      "Backend powered by .NET; handles data storage and authentication",
+    ],
     imgsNum: 3,
     links: {},
   },
