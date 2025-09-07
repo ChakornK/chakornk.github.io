37bd467dca002ee007ac3e4555db2250c7edf92d
ChakornK <120770627+ChakornK@users.noreply.github.com>
2025-09-06 23:55:23 -0700

    feat: add 404 page

---
+++ src/app/404.jsx
@@ -0,0 +1,9 @@
+export default () => (
+  <main className="flex flex-col justify-center items-center bg-black h-screen text-white text-center">
+    <p className="text-6xl">404</p>
+    <p>Page not found</p>
+    <a href="/" className="mt-4">
+      Go home
+    </a>
+  </main>
+);
