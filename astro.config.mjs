508ea47a9001e488a6e73b6e77c9ce78cc3fec51
ChakornK <chakornk2007@gmail.com>
2025-12-27 20:30:14 -0800

    feat: add content to experience page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,14 @@
 import { defineConfig } from "astro/config";
 
 import preact from "@astrojs/preact";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import icon from "astro-icon";
+import yaml from "@rollup/plugin-yaml";
 
 export default defineConfig({
   integrations: [preact(), icon()],
 
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), yaml()],
   },
 });
