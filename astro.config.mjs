c833e68e492cacc8643afb44f62eab1283e8be0a
ChakornK <chakornk2007@gmail.com>
2025-12-28 23:43:42 -0800

    feat: make external link in markdown open in new tab

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,27 @@
 import { defineConfig } from "astro/config";
 
 import preact from "@astrojs/preact";
-import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
+
+import tailwindcss from "@tailwindcss/vite";
 import yaml from "@rollup/plugin-yaml";
+
+import rehypeExternalLinks from "rehype-external-links";
 
 export default defineConfig({
   integrations: [preact(), icon()],
-
   vite: {
     plugins: [tailwindcss(), yaml()],
+  },
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+          rel: "noopener noreferrer",
+        },
+      ],
+    ],
   },
 });
