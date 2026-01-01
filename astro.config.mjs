8e1958fd33b59898c40b0bbfc1155d45982f51c8
ChakornK <chakornk2007@gmail.com>
2026-01-01 12:47:48 -0800

    feat: seo stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
+import fs from "node:fs";
+
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import { imageOptimizer } from "./build/imageOptimizer.js";
+import opengraphImages from "astro-opengraph-images";
+import { ogImage } from "./src/og.tsx";
 
 import tailwindcss from "@tailwindcss/vite";
 import yaml from "@rollup/plugin-yaml";
@@ -10,12 +15,27 @@ import yaml from "@rollup/plugin-yaml";
 import rehypeExternalLinks from "rehype-external-links";
 
 export default defineConfig({
+  site: "https://chakornk.vercel.app",
   integrations: [
+    sitemap(),
     icon(),
     imageOptimizer({
       width: 1344,
       height: 768,
       quality: 50,
+    }),
+    opengraphImages({
+      options: {
+        fonts: [
+          {
+            name: "JetBrains Mono",
+            weight: 400,
+            style: "normal",
+            data: fs.readFileSync("node_modules/@fontsource/jetbrains-mono/files/jetbrains-mono-latin-400-normal.woff"),
+          },
+        ],
+      },
+      render: ogImage,
     }),
   ],
   vite: {
---
+++ src/og.tsx
@@ -0,0 +1,13 @@
+import type { RenderFunctionInput } from "astro-opengraph-images";
+import { twj } from "tw-to-css";
+
+export async function ogImage({ title, description }: RenderFunctionInput) {
+  return Promise.resolve(
+    <div style={twj("h-full w-full flex items-center justify-center bg-gray-50 text-center leading-normal")}>
+      <div style={twj("flex items-center justify-center h-full flex-col gap-2 p-24")}>
+        <h1 style={twj("text-7xl")}>{title}</h1>
+        <div style={twj("text-4xl")}>{description}</div>
+      </div>
+    </div>
+  );
+}
