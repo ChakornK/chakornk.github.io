e40de733940f4ae48ad4d68c3b11ce1061720486
ChakornK <chakornk2007@gmail.com>
2026-01-01 13:04:24 -0800

    fix: increase line spacing in og image

--- src/og.tsx
+++ src/og.tsx
@@ -3,7 +3,7 @@ import { twj } from "tw-to-css";
 
 export async function ogImage({ title, description }: RenderFunctionInput) {
   return Promise.resolve(
-    <div style={twj("h-full w-full flex items-center justify-center bg-gray-50 text-center leading-normal")}>
+    <div style={twj("h-full w-full flex items-center justify-center bg-gray-50 text-center leading-relaxed")}>
       <div style={twj("flex items-center justify-center h-full flex-col gap-2 p-24")}>
         <h1 style={twj("text-7xl")}>{title}</h1>
         <div style={twj("text-4xl")}>{description}</div>
