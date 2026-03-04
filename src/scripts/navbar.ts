4718e184fab3154fac8beb1f1c6b2642bc231e5c
ChakornK <chakornk2007@gmail.com>
2026-03-03 17:15:52 -0800

    refactor: use mask instead of mix blend for navbar highlight

--- src/scripts/navbar.ts
+++ src/scripts/navbar.ts
@@ -1,36 +1,50 @@
 const c = () => document.querySelector(`[data-path="${window.location.pathname.replace(/(?<=[^\^])\/$/, "")}"]`);
+
 let pid: string | null;
+
 document.addEventListener("nav:before-nav", () => {
   const p = c();
-  if (!p) {
-    pid = null;
-  } else {
-    pid = p.getAttribute("data-path");
-  }
+  pid = p ? p.getAttribute("data-path") : null;
 });
+
 const calc = () => {
   const el = document.getElementById("nav-highlight");
   const elc = document.getElementById("nav-highlight-container");
-  if (!el || !elc) return;
+  const clone = document.getElementById("nav-items-clone");
+  if (!el || !elc || !clone) return;
+
   const n = c();
   if (!n) {
     el.style.opacity = "0";
+    clone.style.maskSize = "0px 100%";
     return;
-  } else {
-    el.style.opacity = "1";
   }
+
+  el.style.opacity = "1";
+
   const nb = n.getBoundingClientRect();
   const ncb = elc.getBoundingClientRect();
+  const left = nb.left - ncb.left;
+  const width = nb.right - nb.left;
+
+  let transition = "opacity .15s linear";
   if (pid) {
     const pb = document.querySelector(`[data-path="${pid}"]`)?.getBoundingClientRect();
     if (pb) {
-      el.style.transition = `opacity .15s linear, ${nb.left < pb.left ? "left .1s ease-out, width .1s ease-in-out .025s" : "width .1s ease-out, left .1s ease-in-out .025s"}`;
+      const dir = nb.left < pb.left ? "left .1s ease-out, width .1s ease-in-out .025s" : "width .1s ease-out, left .1s ease-in-out .025s";
+      transition += `, ${dir}`;
     }
-  } else {
-    el.style.transition = "";
   }
-  el.style.left = `${nb.left - ncb.left}px`;
-  el.style.width = `${nb.right - nb.left}px`;
+
+  el.style.transition = transition;
+  el.style.left = `${left}px`;
+  el.style.width = `${width}px`;
+
+  clone.style.maskImage = "linear-gradient(black, black)";
+  clone.style.maskPosition = `${left}px 0`;
+  clone.style.maskSize = `${width}px 100%`;
+  clone.style.transition = transition.replaceAll("left", "mask-position").replaceAll("width", "mask-size");
 };
+
 document.addEventListener("nav:page-load", calc);
 document.fonts.addEventListener("loadingdone", calc);
