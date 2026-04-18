52ef7b14eb4021e57a3dd24684e54d55eae23d9a
ChakornK <chakornk2007@gmail.com>
2026-04-17 23:27:10 -0700

    feat: remove animejs, implement load animation from scratch

--- src/scripts/animate.ts
+++ src/scripts/animate.ts
@@ -1,13 +1,22 @@
-import { animate, stagger } from "animejs";
+const children = document.querySelectorAll("main > *") as NodeListOf<HTMLElement>;
+
+children.forEach((el, i) => {
+  el.style.opacity = "0";
+  el.style.transition = `opacity 300ms ease ${i * 50}ms`;
+
+  requestAnimationFrame(() => {
+    requestAnimationFrame(() => {
+      el.style.opacity = "1";
+    });
+  });
+});
 
 const disableAnimation = () => {
-  document.body.classList.remove("anime-fadein");
+  document.body.classList.remove("intro-fadein");
+  children.forEach((el) => {
+    el.style.transition = "";
+  });
 };
+
 setTimeout(disableAnimation, 1000);
 document.addEventListener("nav:page-load", disableAnimation);
-
-animate("main > *", {
-  opacity: [0, 1],
-  delay: stagger(50),
-  duration: 300,
-});
